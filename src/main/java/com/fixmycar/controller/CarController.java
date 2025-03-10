3b40f8e90774f8099250c5863cff149d98385808
dimin7226 <dimin7226@gmail.com>
2025-03-10 10:36:42 +0300

    First stage the project: 1. Add file .gitignore; 2. Add ResponseStatusException 404

--- src/main/java/com/fixmycar/controller/CarController.java
+++ src/main/java/com/fixmycar/controller/CarController.java
@@ -4,12 +4,16 @@ import com.fixmycar.model.Car;
 import com.fixmycar.service.CarService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequestMapping("/cars")
 public class CarController {
 
     private final CarService carService;
@@ -19,20 +23,29 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping("/cars/{id}")
+    @GetMapping("/{id}")
     public Car getCarById(@PathVariable Long id) {
-        return carService.getCarById(id).orElseThrow(() -> new RuntimeException("Car not found"));
+        return carService.getCarById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Car not found"));
     }
 
-    @GetMapping("/cars")
+    @GetMapping
     public List<Car> getAllCars() {
         return carService.getAllCars();
     }
 
-    @GetMapping("/cars/filter")
+    @GetMapping("/filter")
     public List<Car> getCarsByFilter(
             @RequestParam(required = false) String brand,
             @RequestParam(required = false) String model) {
-        return carService.getCarsByFilter(brand, model);
+        List<Car> cars = carService.getCarsByFilter(brand, model);
+
+        if (cars.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "No cars found with specified filters");
+        }
+
+        return cars;
     }
 }

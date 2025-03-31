a3750fe2c391df9e2ee5189ebdec55d11e2e02ed
dimin7226 <dimin7226@gmail.com>
2025-03-31 09:53:49 +0300

    Fix checkstyle and SonarCloud errors

--- src/main/java/com/fixmycar/controller/Constants.java
+++ src/main/java/com/fixmycar/controller/Constants.java
@@ -1,5 +1,10 @@
 package com.fixmycar.controller;
 
-public class Constants {
+public final class Constants {
     public static final String REQUEST_NOT_FOUND_ID = "Request not found with id ";
+
+    private Constants() {
+        throw new UnsupportedOperationException(
+                "This is a utility class and cannot be instantiated");
+    }
 }

8ff9ab543aa3dcbf27cbf1e2378ded9f6eee09dd
dimin7226 <dimin7226@gmail.com>
2025-11-02 16:41:17 +0300

    Fix acsess errors

--- src/main/java/com/fixmycar/config/WebConfig.java
+++ src/main/java/com/fixmycar/config/WebConfig.java
@@ -1,11 +1,8 @@
 package com.fixmycar.config;
 
-import com.fixmycar.service.VisitInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,12 +13,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                // Разрешаем CORS для всех путей backend
-                registry.addMapping("/**")
-                        // Разрешаем доступ с фронтенда
-                        // Если фронтенд на Nginx локально: "http://192.168.10.100"
-                        // Если фронтенд на другом домене, укажи его URL
-                        .allowedOrigins("http://192.168.10.100", "https://fixmycar-frontend.onrender.com")
+                registry.addMapping("/**") // все эндпоинты
+                        .allowedOrigins("https://fixmycar.onrender.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }

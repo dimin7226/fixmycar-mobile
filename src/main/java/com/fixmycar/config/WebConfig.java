db12c9b614d6116a35e27860369c480a2966b897
dimin7226 <dimin7226@gmail.com>
2025-11-17 23:42:32 +0300

    Fix nginx config

--- src/main/java/com/fixmycar/config/WebConfig.java
+++ src/main/java/com/fixmycar/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // все эндпоинты
-                        .allowedOrigins("https://fixmycar.onrender.com/")
+                        .allowedOrigins("https://fixmycar-frontend.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
